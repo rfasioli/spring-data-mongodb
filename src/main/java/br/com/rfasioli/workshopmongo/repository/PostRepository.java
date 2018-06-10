17ed74a4e274cccab5c75b859286bb3c8e8ae773
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2018-06-10 16:27:26 -0300

    busca post por parte do titulo

--- src/main/java/br/com/rfasioli/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/rfasioli/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package br.com.rfasioli.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import br.com.rfasioli.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	public List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
--- src/main/java/br/com/rfasioli/workshopmongo/resource/PostResource.java
+++ src/main/java/br/com/rfasioli/workshopmongo/resource/PostResource.java
@@ -1,13 +1,17 @@
 package br.com.rfasioli.workshopmongo.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rfasioli.workshopmongo.domain.Post;
+import br.com.rfasioli.workshopmongo.resource.util.URL;
 import br.com.rfasioli.workshopmongo.service.PostService;
 
 @RestController
@@ -21,6 +25,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		List<Post> posts = postService.findByTitle(URL.decodeParam(text));
+		return ResponseEntity.ok().body(posts);
 	}
 	
 }
