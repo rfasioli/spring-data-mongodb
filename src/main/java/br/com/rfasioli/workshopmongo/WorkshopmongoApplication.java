8e297add6b644081080a7ab764066132e5da5546
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2018-06-10 13:56:08 -0300

    initial commit

---
+++ src/main/java/br/com/rfasioli/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,12 @@
+package br.com.rfasioli.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+}
