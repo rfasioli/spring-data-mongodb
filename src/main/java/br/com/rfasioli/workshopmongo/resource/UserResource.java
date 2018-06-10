6bdda91a46d26edd2f9ec1fff4261efbc0dac720
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2018-06-10 14:51:56 -0300

    Obtendo usuario por id

--- src/main/java/br/com/rfasioli/workshopmongo/resource/UserResource.java
+++ src/main/java/br/com/rfasioli/workshopmongo/resource/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +27,12 @@ public class UserResource {
 		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDto> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		UserDto userDto = new UserDto(user);
+		return ResponseEntity.ok().body(userDto);
+	}
+	
 }
--- src/main/java/br/com/rfasioli/workshopmongo/service/UserService.java
+++ src/main/java/br/com/rfasioli/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package br.com.rfasioli.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.rfasioli.workshopmongo.domain.User;
 import br.com.rfasioli.workshopmongo.repository.UserRepository;
+import br.com.rfasioli.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
